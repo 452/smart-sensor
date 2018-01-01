234c047f7049f5e48b27f997e0a1e9c7ec9a2b7f
emetemunoy <sp.titan@gmail.com>
2018-01-01 13:41:13 +0200

    refactoring, sensor replacing BMP280 -> BME280

--- smart-sensor.ino
+++ smart-sensor.ino
@@ -1,17 +1,20 @@
 /*
    Articles for next update
    http://www.esp8266.com/viewtopic.php?f=29&t=4209
+   //return "<sensor>\n<measurements>\n<temperature>" + String(bme.readTemperature(), 1) + "</temperature>" + "\n<pressure>" + String(bme.readPressure(), 1) + "</pressureBMP280>" + "\n<altitudeBMP280>" + String(bme.readAltitude(1013.25), 1) + "</altitudeBMP280>" + "\n<humidityDTH11>" + String(dht.readHumidity(), 1) + "</humidityDTH11>" + "\n<temperatureDTH11>" + String(dht.readTemperature(), 1) + "</temperatureDTH11>\n</measurements>\n<systemInfo>\n<rssi>" + String(WiFi.RSSI(), DEC) + "</rssi>\n<vcc>" + String(ESP.getVcc() / 1024.00f, DEC) + "</vcc>\n<serialNumber>" + serialNumber + "</serialNumber>\n<firmwareVersion>" + firmwareVersion + "</firmwareVersion>\n<chipId>" + ESP.getChipId() + "</chipId>\n<flashChipId>" + ESP.getFlashChipId() + "</flashChipId>\n<macAddress>" + WiFi.macAddress() + "</macAddress></systemInfo></sensor>";
+   //Adafruit_BME280 sensor(CS, MOSI, MISO, SCK);
+   //DHT dht(D3, DHT11);
+   //#include <DHT.h>/
+   #include <SPI.h>
 */
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ArduinoOTA.h>
 #include <ESP8266HTTPUpdateServer.h>
 #include <ESP8266WebServer.h>
-#include <DHT.h>
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
 
 //#define DEBUG
 
@@ -19,19 +22,17 @@
 #define CLK  D5
 #define MISO D6
 #define MOSI D7
+#define SENSOR_ADDRESS 0x76
 
-//Adafruit_BMP280 bme();
-Adafruit_BMP280 bme(CS, MOSI, MISO, SCK);
-
-DHT dht(D3, DHT11);
+Adafruit_BME280 sensor;
 
 const char* ssid = "}RooT{";
 const char* password = "";
 const char* serialNumber = "1";
-const char* firmwareVersion = "201711140730";
+const char* firmwareVersion = "201801010730";
 const char* host = "smart-sensor";
-const char* bmp280ErrorInfo = "Could not find a valid BMP280 sensor, check wiring!";
-boolean bmp280Initialized;
+const char* sensorErrorInfo = "Could not find a valid BME280 sensor, check wiring!";
+boolean sensorInitialized;
 
 MDNSResponder mdns;
 ESP8266WebServer server(80);
@@ -42,10 +43,10 @@ void handleRoot() {
 }
 
 String getSensorDataXML() {
-  if (bmp280Initialized) {
-    return "<sensor>\n<measurements>\n<temperatureBMP280>" + String(bme.readTemperature(), 1) + "</temperatureBMP280>" + "\n<pressureBMP280>" + String(bme.readPressure(), 1) + "</pressureBMP280>" + "\n<altitudeBMP280>" + String(bme.readAltitude(1013.25), 1) + "</altitudeBMP280>" + "\n<humidityDTH11>" + String(dht.readHumidity(), 1) + "</humidityDTH11>" + "\n<temperatureDTH11>" + String(dht.readTemperature(), 1) + "</temperatureDTH11>\n</measurements>\n<systemInfo>\n<rssi>" + String(WiFi.RSSI(), DEC) + "</rssi>\n<vcc>" + String(ESP.getVcc() / 1024.00f, DEC) + "</vcc>\n<serialNumber>" + serialNumber + "</serialNumber>\n<firmwareVersion>" + firmwareVersion + "</firmwareVersion>\n<chipId>" + ESP.getChipId() + "</chipId>\n<flashChipId>" + ESP.getFlashChipId() + "</flashChipId>\n<macAddress>" + WiFi.macAddress() + "</macAddress></systemInfo></sensor>";
+  if (sensorInitialized) {
+    return "<sensor>\n<measurements>\n<temperature>" + String(sensor.readTemperature(), 1) + "</temperature>" + "\n<pressure>" + String(sensor.readPressure(), 1) + "</pressure>" + "\n<altitude>" + String(sensor.readAltitude(1013.25), 1) + "</altitude>" + "\n<humidity>" + String(sensor.readHumidity(), 1) + "</humidity>\n</measurements>\n<systemInfo>\n<rssi>" + String(WiFi.RSSI(), DEC) + "</rssi>\n<vcc>" + String(ESP.getVcc() / 1024.00f, DEC) + "</vcc>\n<serialNumber>" + serialNumber + "</serialNumber>\n<firmwareVersion>" + firmwareVersion + "</firmwareVersion>\n<chipId>" + ESP.getChipId() + "</chipId>\n<flashChipId>" + ESP.getFlashChipId() + "</flashChipId>\n<macAddress>" + WiFi.macAddress() + "</macAddress></systemInfo></sensor>";
   } else {
-    return bmp280ErrorInfo;
+    return sensorErrorInfo;
   }
 }
 
@@ -96,11 +97,11 @@ void setup() {
   ArduinoOTA.setHostname(host);
   httpUpdater.setup(&server);
   server.begin();
-  if (bme.begin()) {//0x76
-    bmp280Initialized = true;
+  if (sensor.begin(SENSOR_ADDRESS)) {
+    sensorInitialized = true;
   } else {
 #ifdef DEBUG
-    Serial.println(bmp280ErrorInfo);
+    Serial.println(sensorErrorInfo);
 #endif
   }
 #ifdef DEBUG
